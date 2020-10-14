2bdc8ca3bd75f1ac205ec222edd0e35ff2658427
Lovisa <lovfor@kth.se>
2020-10-14 14:38:04 +0200

    context and tabnavigator

--- App.js
+++ App.js
@@ -1,47 +1,70 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, createContext } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { LoginScreen, HomeScreen, RegistrationScreen } from './Screens'
 import {firebase} from './Screens/Firebase/config'
 import {decode, encode} from 'base-64'
+import AddScreen from './Screens/AddScreen/AddScreen';
+
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
+export const CurrentUserContext = React.createContext();
+
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
 
 export default function App() {
 
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
   const [initializing,setInitializing]=useState(true)
+
+  createBottomTabs = () => {
+    return(
+      <Tab.Navigator>
+        <Tab.Screen name = 'Home' component = {HomeScreen}/>
+        <Tab.Screen name = 'Add' component = {AddScreen}/>
+      </Tab.Navigator>
+    )
+  }
  
   useEffect(()=>{
+    
       const subscriber=firebase.auth().onAuthStateChanged((user)=>{  
-        console.log(user)
         setUser(user)
         setInitializing(false)
       })
-      console.log(user)
+      //console.log(user)
+      //console.log(user.email)
+
       return subscriber
-    },[])
+    }, [])
 
   if (initializing) return null  //Here you may use an Activity indicator
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {user ? (
-          <Stack.Screen name="Home">
-          {props => <HomeScreen {...props} extraData={user} />}
-          </Stack.Screen>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-            </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CurrentUserContext.Provider value = {{user}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {user ? (
+            <Stack.Screen name="Home" children={createBottomTabs}>
+            {/*{props => <HomeScreen {...props} extraData={user} />}*/}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+              </>
+          )}
+          </Stack.Navigator>
+      </NavigationContainer>
+    </CurrentUserContext.Provider>
+
   );
 }
